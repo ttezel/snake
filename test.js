96861265b33eb25980bef14a1d4a8066261b4a2c
Tolga Tezel <tolgatezel11@gmail.com>
2012-05-28 12:39:37 -0400

    add alternate heuristic tests

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var assert = require('assert')
   , Snake = require('./snake')
+  , Map = require('./map')
 
 var maze = [
   [0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0]
@@ -32,28 +33,31 @@ var maze = [
 var snake = new Snake()
 
 var cases = [
-//   { opts: { maze: maze, start: [2,11], end: [23,19] }, method: 'breadthFirst' }
-// , { opts: { maze: maze, start: [2,11], end: [2,21] }, method: 'breadthFirst' }
-// , { opts: { maze: maze, start: [0,0], end: [24,24] }, method: 'breadthFirst' }
-// , { opts: { maze: maze, start: [2,11], end: [23,19] }, method: 'depthFirst' }
-// , { opts: { maze: maze, start: [2,11], end: [2,21] }, method: 'depthFirst' }
-// , { opts: { maze: maze, start: [0,0], end: [24,24] }, method: 'depthFirst' }
-
-{ opts: { maze: maze, start: [2,11], end: [23,19] }, method: 'alternate' }
-// , { opts: { maze: maze, start: [2,11], end: [2,21] }, method: 'alternate' }
-// , { opts: { maze: maze, start: [0,0], end: [24,24] }, method: 'alternate' }
+   { opts: { maze: maze, start: [2,11], end: [23,19], heuristic: 'breadthFirst' } }
+, { opts: { maze: maze, start: [2,11], end: [2,21], heuristic: 'breadthFirst' } }
+, { opts: { maze: maze, start: [0,0], end: [24,24], heuristic: 'breadthFirst' } }
+, { opts: { maze: maze, start: [2,11], end: [23,19], heuristic: 'depthFirst' } }
+, { opts: { maze: maze, start: [2,11], end: [2,21], heuristic: 'depthFirst' } }
+, { opts: { maze: maze, start: [0,0], end: [24,24], heuristic: 'depthFirst' } }
+, { opts: { maze: maze, start: [2,11], end: [23,19], heuristic: 'linear' } }
+, { opts: { maze: maze, start: [2,11], end: [2,21], heuristic: 'linear' } }
+, { opts: { maze: maze, start: [0,0], end: [24,24], heuristic: 'linear' } }
+, { opts: { maze: maze, start: [2,11], end: [23,19], heuristic: 'manhattan' } }
+, { opts: { maze: maze, start: [2,11], end: [2,21], heuristic: 'manhattan' } }
+, { opts: { maze: maze, start: [0,0], end: [24,24], heuristic: 'manhattan' } }
 ]
 
 cases.forEach(function (test, index) {
   var opts = test.opts
-  var result = snake[test.method](opts)
+  var result = snake.solve(opts)
   assert.strictEqual(result.msg, 'found exit', 'expected message to be `found exit`')
   assert.strictEqual(result.status, 1, 'expected status too be 1')
   assert(Array.isArray(result.route), 'expected route to be array')
   console.log(
-    '\nTest#%s: `%s`. start: [%s] end [%s]. elapsed: %s ms. route (cost %s):\n\n%s\n'
-  , index+1, test.method, opts.start, opts.end, result.elapsed, result.cost, result.route.join(' -> ')
+    '\nTest#%s: %s. start: [%s] end [%s]. elapsed: %s ms. route (cost %s):\n\n%s\n'
+  , index+1, opts.heuristic, opts.start, opts.end, result.elapsed, result.cost, result.route.join(' -> ')
   )
+
+  var map = new Map(test, result).render()
 })
 console.log('\n\nALL TESTS PASS :)\n-------------------')
-
